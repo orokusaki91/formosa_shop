18edf3f500461116976bbf25d313185e79392f97
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2019-06-20 18:51:16 +0200

    Initial commit

---
+++ config/themes.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'default' => 'Custom',
+
+    'themes' => [
+        // 'default' => [
+        //     'views_path' => 'resources/themes/default/views',
+        //     'assets_path' => 'public/themes/default/assets',
+        //     'name' => 'Default'
+        // ],
+
+        'custom' => [
+            'views_path' => 'resources/views/themes/custom',
+            'assets_path' => 'public/themes/custom/assets',
+            'name' => 'Custom',
+            // 'parent' => 'default'
+        ]
+    ]
+];
